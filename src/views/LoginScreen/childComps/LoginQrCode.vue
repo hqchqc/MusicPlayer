a4a59c348f61c791a8d66b58d4feaade1a69bd6d
hqchqc <github@hqchqc.com>
2022-04-23 15:29:27 +0800

    :lipstick: 重装后测试文件格式化

--- src/views/LoginScreen/childComps/LoginQrCode.vue
+++ src/views/LoginScreen/childComps/LoginQrCode.vue
@@ -5,7 +5,7 @@ import { ref } from "vue";
 const isShow = ref(false);
 const imgSrc = ref("");
 
-const { getQrCodeKey, getQrCode, getQrCodeStatus } = loginApi;
+const { getQrCodeKey, getQrCode } = loginApi;
 
 const handleQrCode = async () => {
   isShow.value = true;
