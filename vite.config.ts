10dbe184dec4e63269e18169d98dba7758e8675e
hqchqc <734986933@qq.com>
2023-04-06 23:56:02 +0800

    fix: update version

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 // import Preview from 'vite-plugin-vue-component-preview'
 import Vue from '@vitejs/plugin-vue'
