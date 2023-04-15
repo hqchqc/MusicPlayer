22831e896a12657b097b8c1b965b9079bd812158
hqchqc <734986933@qq.com>
2023-04-15 09:54:54 +0800

    fix: 修复volar报错的问题及更改接口

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -18,7 +18,8 @@ declare module '*.vue' {
 // TODO: https://github.com/vuejs/pinia/issues/2098
 declare module 'pinia' {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  export function acceptHMRUpdate(initialUseStore: StoreDefinition | any, hot: any): (newModule: any) => any;
+  export function acceptHMRUpdate(initialUseStore: StoreDefinition | any, hot: any): (newModule: any) => any
   export function defineStore<Id, S, G, A>(id, options): StoreDefinition<Id, S, G, A>
+  export function storeToRefs<SS>(store): ToRefs<StoreState<SS> & StoreGetters<SS> & PiniaCustomStateProperties<StoreState<SS>>>
   export const createPinia: Pinia
 }
